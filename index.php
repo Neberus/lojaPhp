51937aa92788d7ced86fb363e2b6874977765b68
unknown <Digital@DH-A10-24.dhbrasil.digitalhouse.com>
2019-09-27 12:03:56 -0300

    Criando a loja

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    $nomeSistema = "Cursos Show!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <header class="d-flex justify-content-between align-items-center p-3">
+        <h1 id="logo"><?php echo $nomeSistema; ?></h1>
+        <nav>
+            <ul class="nav">
+                <li class="nav-item">Cursos</li>
+                <li class="nav-item">Login</li>
+                <li class="nav-item">Cadastrar</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
+                    <!-- <?php
+                        // $nome = "Yuri";
+                        // $idade = 10;
+                        // $usuario = ["Yuri", "Coelho", 21, ["comida", "viagem", "jogos"]]; //array dentro de array//
+
+                        // $usuarioDois = ["nome" => "Yuri", "sobrenome" => "Coelho"]; //array associativo//
+
+                        // echo "Hello World!";
+
+                        // var_dump($usuario);
+
+                        // $usuario[] = "Cozinhar";
+
+                        // echo "<br>";
+
+                        // var_dump($usuario);
+
+                        // echo "<br>";
+
+                        // echo $usuario[3][2];
+
+                        // echo "<br>";
+
+                        // echo $usuario[0].$usuario[1];
+
+                        // echo "<br>";
+
+                        // echo $usuarioDois["sobrenome"];
+                    ?> -->
